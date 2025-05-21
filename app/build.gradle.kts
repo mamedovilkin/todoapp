7ed54aaa7411447ed22ba316552389e42f8c80f3
Ilkin Mamedov <ilkinmamedov0208@gmail.com>
2025-05-21 14:55:51 +0300

    Version 1.3 (Release)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.mamedovilkin.todoapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
